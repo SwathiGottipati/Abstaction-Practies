64a18e108094b32c05bbecd5acd9ff3d32b5958d
SwathiGottipati <swathitesting9@gmail.com>
2024-02-16 19:58:41 +0000

    first commit

---
+++ abstract practies/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Application
+{
+    abstract class Coffee
+    {
+        public abstract void Hotchocolate();
+        public void Taste()
+        {
+            Console.WriteLine("hotchocolate is sweet");
+        }
+
+    }
+    class Costa : Coffee
+    {
+        public override void Hotchocolate()
+        {
+            Console.WriteLine("costa is brand");
+        }
+
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Costa Brand = new Costa();
+            Brand.Taste();
+            Brand.Hotchocolate();
+        }
+
+
+    }
+}
+
